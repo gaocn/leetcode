2836c6ed807f1ebfd75f79b84ef489fca074d228
gaocn <gaowenwencn@163.com>
2015-04-10 10:38:03 +0800

    Update MinimumWindowSubstring.java

--- MinimumWindowSubstring.java
+++ MinimumWindowSubstring.java
@@ -1,5 +1,5 @@
 public String minWindow(String S, String T) {
-    int[] hasFound   = new int[256];
+	 int[] hasFound   = new int[256];
 	int[] needToFind = new int[256];
 	for(int i=0; i < T.length(); i++)
 		++needToFind[T.charAt(i)];
@@ -18,20 +18,20 @@ public String minWindow(String S, String T) {
 			++foundCharCount;
 		
 		if(foundCharCount == T.length()) {
-            while (needToFind[S.charAt(begin)] == 0 ||
-                 hasFound[S.charAt(begin)] > needToFind[S.charAt(begin)]) {
-    					if(hasFound[S.charAt(begin)] > needToFind[S.charAt(begin)])
-    					--hasFound[S.charAt(begin)];
-    					++begin;
-    		}
-			int len = end - begin +1;
-			if(len < minWindowLen) {
-				minBegin 	 = begin;
-				minEnd   	 = end;
-				minWindowLen = len;
+			while (needToFind[S.charAt(begin)] == 0 ||
+	         		hasFound[S.charAt(begin)] > needToFind[S.charAt(begin)]) {
+	    			if(hasFound[S.charAt(begin)] > needToFind[S.charAt(begin)])
+	    				--hasFound[S.charAt(begin)];
+	    			++begin;
+	    	}
+		int len = end - begin +1;
+		if(len < minWindowLen) {
+			minBegin 	 = begin;
+			minEnd   	 = end;
+			minWindowLen = len;
 		 }
-    	//置为0进行下一次搜索??错误，不能添加这句话，为什么？？
-        //foundCharCount = 0;
+	    //置为0进行下一次搜索??错误，不能添加这句话，为什么？？
+	//foundCharCount = 0;
 	}
 	return minWindowLen <= S.length()?S.substring(minBegin, minEnd+1):"";
 }
